bccf6de52576220cb66522cbb0bc42f052bbb721
Bassotto-Guido <danieleguidetti06@gmail.com>
2023-08-08 22:50:46 +0200

    Aggiunte interazioni semplici, grab, dirette e con ray

---
+++ Assets/Script/ActivateGrabRay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class ActivateRayGrab : MonoBehaviour
+{
+    public GameObject leftGrabRay;
+    public GameObject rightGrabRay;
+
+    public XRDirectInteractor leftDirectGrab;
+    public XRDirectInteractor rightDirectGrab;
+
+    void Update()
+    {
+        leftGrabRay.SetActive(leftDirectGrab.interactablesSelected.Count == 0);
+        rightGrabRay.SetActive(rightDirectGrab.interactablesSelected.Count == 0);
+    }
+}
