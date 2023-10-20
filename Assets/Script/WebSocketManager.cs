4887b2c29a44b0e3d8bd1bb190079d5568665b5a
Bassotto-Guido <danieleguidetti06@gmail.com>
2023-10-20 17:26:00 +0200

    fix little bug when you come back to the first scene and then exit

--- Assets/Script/WebSocketManager.cs
+++ Assets/Script/WebSocketManager.cs
@@ -148,6 +148,7 @@ public class WebSocketManager : MonoBehaviour
 
     void OnDestroy()
     {
+        Instance = null;
         OnEmitToClient?.Invoke(WebSocketEvent.OnUnityExited, "");
         server.Stop();
     }
